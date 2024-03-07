addad5e950d4e71576c454b8c8aed827066b2af8
Dinh Van Toan <vantoanab99@gmail.com>
2024-03-07 10:37:46 +0700

    assignment 3

---
+++ models/dishes.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;	var commentSchema = new Schema({
+	    rating:  {
+	        type: Number,
+	        min: 1,
+	        max: 5,
+	        required: true
+	    },
+	    comment:  {
+	        type: String,
+	        required: true
+	    },
+	    author: {
+	        type: mongoose.Schema.Types.ObjectId,
+	        ref: 'User'
+	    }
+	}, {
+	    timestamps: true
+	});
+	
+    
+
+var dishSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    description:{
+        type: String,
+        require: true,
+    },
+    category: {
+        type: String,
+       
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+       
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    comments: [commentSchema]
+},{ 
+    timestamps: true
+});
+var Dishes = mongoose.model('dish',dishSchema);
+module.exports = Dishes
